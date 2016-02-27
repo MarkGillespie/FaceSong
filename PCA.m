bdb01d77e20fbb31e3293bab156853b71eba725b
Kevin Shu <shu.kevins@gmail.com>
2016-02-27 02:51:21 -0800

    Finished the EigenFace function

--- PCA.m
+++ PCA.m
@@ -1,21 +1,12 @@
-LoadFaces[n_] := With[{nb = EvaluationNotebook[]}, Import /@ Take[FileNames[FileNameJoin[{NotebookDirectory[], "testfaces", "*.jpg"}]], n]]
+LoadFaces[n_] := ColorConvert[ Import /@ Take[FileNames[FileNameJoin[{NotebookDirectory[], "testfaces", "*.jpg"}]], n], "Grayscale"]
 
 FlattenImage[image:(_Graphics | _Image)]:=N[Flatten[ImageData[ImageResize[image,100]]]];
 
-EigenImageElements[images_List, frac_ : 0.5] := 
-     Module[{imgMatrix = FlattenImage /@ images, imgMatrixAdj, imgAverage, eigenVecs},
-        imgAverage = N[Total[imgMatrix] / Length[imgMatrix]];
-        imgMatrixAdj = (# - imgAverage) & /@ imgMatrix;
-        eigenVecs = Eigenvectors[
-            Dot[
-                imgMatrixAdj,
-                Transpose[imgMatrixAdj]
-            ],
-            Ceiling[
-                frac*Length[imgMatrixAdj]
-            ]
-        ];
-        imgMatrixAdj = Dot[eigenVecs, imgMatrix];
-        {imgMatrixAdj, imgAverage, eigenVecs}
+EigenFaces[faces_, ncomps_] := 
+ Module[{imgMatrix, avg},
+    imgMatrix = FlattenImage /@ faces;
+    avg  = N[Total[imgMatrix]]/Length[imgMatrix];
+    Dot[Transpose[imgMatrix], #] & /@ Eigenvectors[Covariance[Transpose[imgMatrix]], ncomps]
 ]
 
+FormatFace[face_] := Graphics[Raster[Partition[face, 100]]]
