9ed8a8533a83d6851f46259ebcecd6443d47006c
Mark Gillespie <mark.gillespie81@verizon.net>
2016-02-26 22:38:24 -0800

    formatted PCA.m indentation

--- PCA.m
+++ PCA.m
@@ -1,14 +1,20 @@
-
-
-LoadFaces[n_] = With[{nb = EvaluationNotebook[]}, Import /@ Take[FileNames[FileNameJoin[{NotebookDirectory[], "testfaces", "*.jpg"}]], n]]
+LoadFaces[n_] := With[{nb = EvaluationNotebook[]}, Import /@ Take[FileNames[FileNameJoin[{NotebookDirectory[], "testfaces", "*.jpg"}]], n]]
 
 FlattenImage[image:(_Graphics | _Image)]:=N[Flatten[ImageData[ImageResize[image,100]]]];
 
 EigenImageElements[images_List, frac_ : 0.5] := 
-     Module[{imgMatrix = imageVector /@ images, imgMatrixAdj, imgAverage, eigenVecs},
+     Module[{imgMatrix = FlattenImage /@ images, imgMatrixAdj, imgAverage, eigenVecs},
         imgAverage = N[Total[imgMatrix] / Length[imgMatrix]];
         imgMatrixAdj = (# - imgAverage) & /@ imgMatrix;
-        eigenVecs = Eigenvectors[Dot[imgMatrixAdj, Transpose[imgMatrixAdj]], Ceiling[frac*Length[imgMatrixAdj]]];
+        eigenVecs = Eigenvectors[
+            Dot[
+                imgMatrixAdj,
+                Transpose[imgMatrixAdj]
+            ],
+            Ceiling[
+                frac*Length[imgMatrixAdj]
+            ]
+        ];
         imgMatrixAdj = Dot[eigenVecs, imgMatrix];
         {imgMatrixAdj, imgAverage, eigenVecs}
 ]
